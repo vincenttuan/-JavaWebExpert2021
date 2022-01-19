580b431797fe3502d9cd881882d996203bf558fc
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-19 09:51:57 +0800

    Add files via upload

--- src/main/java/web/mvc/course/service/CourseService.java
+++ src/main/java/web/mvc/course/service/CourseService.java
@@ -1,5 +1,6 @@
 package web.mvc.course.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,16 @@ import web.mvc.course.model.Course;
 import web.mvc.spec.IService;
 
 public class CourseService implements IService<Course> {
-
+	private static List<Course> courses;
+	static { // 類別資源初始資料實作區
+		courses = new ArrayList<>();
+		courses.add(new Course(1001, "程式語言", 3));
+		courses.add(new Course(2001, "企業管理", 2));
+		courses.add(new Course(3001, "統計學", 3));
+		courses.add(new Course(4001, "商用英文", 1));
+		courses.add(new Course(5001, "資料庫", 3));
+	}
+	
 	@Override
 	public List<Course> queryAll() {
 		// TODO Auto-generated method stub
