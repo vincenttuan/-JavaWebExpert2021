a4164e80cf30ad22f64af967f09662a96f8700d4
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-19 09:41:15 +0800

    Add files via upload

--- src/main/java/web/mvc/course/service/CourseService.java
+++ src/main/java/web/mvc/course/service/CourseService.java
@@ -1,10 +1,47 @@
 package web.mvc.course.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import web.mvc.course.model.Course;
-import web.mvc.student.model.Student;
+import web.mvc.spec.IService;
 
-public class CourseService {
+public class CourseService implements IService<Course> {
+
+	@Override
+	public List<Course> queryAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Course t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Optional<Course> getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Course t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isIdExists(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
