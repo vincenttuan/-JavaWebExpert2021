32bc659ebedd111ab5235a95c69c78f825bb1ff8
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-19 11:24:46 +0800

    Update CourseController.java

--- src/main/java/web/mvc/course/controller/CourseController.java
+++ src/main/java/web/mvc/course/controller/CourseController.java
@@ -20,6 +20,67 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/mvc/course/*")
 public class CourseController extends HttpServlet {
 	
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	private void form(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+	private void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	private void queryall(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	private void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String httpMethod = req.getMethod();
+		String pathInfo = req.getPathInfo();
+		System.out.println(httpMethod + " /mvc/course" + pathInfo);
+		if(httpMethod.equalsIgnoreCase("GET")) {
+			switch (pathInfo) {
+				case "/delete":
+					delete(req, resp);
+					break;
+				case "/form":
+					form(req, resp);
+					break;
+				case "/get":
+					get(req, resp);
+					break;
+				case "/queryall":
+					queryall(req, resp);
+					break;	
+				default:
+					resp.sendError(500, "網路連結路徑錯誤");
+			}
+		} else if(httpMethod.equalsIgnoreCase("POST")) {
+			switch (pathInfo) {
+				case "/add":
+					add(req, resp);
+					break;
+				case "/update":
+					update(req, resp);
+					break;	
+				default:
+					resp.sendError(500, "網路連結路徑錯誤");
+			}
+		} else {
+			resp.sendError(500, "HTTP 方法錯誤 (本站只支援 GET 或 POST)");
+		}
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doHandle(req, resp);
@@ -30,35 +91,5 @@ public class CourseController extends HttpServlet {
 		doHandle(req, resp);
 	}
 	
-	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String httpMethod = req.getMethod();
-		String pathInfo = req.getPathInfo();
-		System.out.println(httpMethod + " /mvc/course" + pathInfo);
-		if(httpMethod.equalsIgnoreCase("GET")) {
-			switch (pathInfo) {
-				case "/delete":
-					break;
-				case "/form":
-					break;
-				case "/get":
-					break;
-				case "/queryall":
-					break;	
-				default:
-					resp.sendError(500, "網路連結路徑錯誤");
-			}
-		} else if(httpMethod.equalsIgnoreCase("POST")) {
-			switch (pathInfo) {
-				case "/add":
-					break;
-				case "/update":
-					break;	
-				default:
-					resp.sendError(500, "網路連結路徑錯誤");
-			}
-		} else {
-			resp.sendError(500, "HTTP 方法錯誤 (本站只支援 GET 或 POST)");
-		}
-	}
 	
 }
