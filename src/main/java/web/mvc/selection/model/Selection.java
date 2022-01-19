48668bf7193cffa24c1d926926dbbaa7423f79bd
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-19 16:32:19 +0800

    Add files via upload

--- src/main/java/web/mvc/selection/model/Selection.java
+++ src/main/java/web/mvc/selection/model/Selection.java
@@ -1,26 +1,38 @@
 package web.mvc.selection.model;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import web.mvc.course.model.Course;
 
 public class Selection {
 	
 	private Integer studentId;
-	private Set<Course> courses;
+	private List<Course> courses = new ArrayList<>();
 	
 	public Integer getStudentId() {
 		return studentId;
 	}
+
+
 	public void setStudentId(Integer studentId) {
 		this.studentId = studentId;
 	}
-	public Set<Course> getCourses() {
+
+
+	public List<Course> getCourses() {
 		return courses;
 	}
-	public void setCourses(Set<Course> courses) {
+
+
+	public void setCourses(List<Course> courses) {
 		this.courses = courses;
 	}
+	
+	public void addCourse(Course course) {
+		courses.add(course);
+	}
+
 	@Override
 	public String toString() {
 		return "Selection [studentId=" + studentId + ", courses=" + courses + "]";
---
+++ src/main/java/web/mvc/selection/service/SelectionService.java
@@ -0,0 +1,41 @@
+package web.mvc.selection.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import web.mvc.course.model.Course;
+import web.mvc.course.service.CourseService;
+import web.mvc.selection.model.Selection;
+import web.mvc.student.model.Student;
+
+public class SelectionService {
+	
+	private static List<Selection> selections;
+	private CourseService courseService = new CourseService();
+	static {
+		selections = new ArrayList<>();
+	}
+	
+	public List<Selection> queryAll() {
+		return selections;
+	}
+	
+	public void addOrUpdate(Integer studentId, List<Integer> course_ids) {
+		Optional<Selection> optSelection = selections.stream()
+				.filter(s -> s.getStudentId().intValue() == studentId.intValue())
+				.findAny();
+		if(optSelection.isPresent()) {
+			// 先移除
+			selections.remove(optSelection.get());
+		}
+		// 新增
+		Selection selection = new Selection();
+		selection.setStudentId(studentId);
+		for(Integer course_id : course_ids) {
+			Course course = courseService.getById(studentId).get();
+			selection.addCourse(course);
+		}
+	}
+	
+}
