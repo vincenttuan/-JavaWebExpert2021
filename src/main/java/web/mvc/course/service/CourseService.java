557738b676c863fafe8bdb4b0efed2012013e383
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-19 10:05:30 +0800

    Add files via upload

--- src/main/java/web/mvc/course/service/CourseService.java
+++ src/main/java/web/mvc/course/service/CourseService.java
@@ -3,9 +3,11 @@ package web.mvc.course.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import web.mvc.course.model.Course;
 import web.mvc.spec.IService;
+
 
 public class CourseService implements IService<Course> {
 	private static List<Course> courses;
@@ -17,41 +19,69 @@ public class CourseService implements IService<Course> {
 		courses.add(new Course(4001, "商用英文", 1));
 		courses.add(new Course(5001, "資料庫", 3));
 	}
-	
+
+	// 全部查詢
 	@Override
 	public List<Course> queryAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return courses;
 	}
 
+	// 新增
 	@Override
-	public int add(Course t) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int add(Course course) {
+		if (isIdExists(course.getId())) {
+			System.out.printf("CourseService.add() -> id: %d 已存在", course.getId());
+			return 0; // 0:失敗
+		}
+		courses.add(course);
+		return 1; // 1:成功
 	}
 
+	// 單筆查詢：根據 id 來取得 student 物件
 	@Override
 	public Optional<Course> getById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		// parallel 平行查詢/多核心同步運算
+		return courses.stream().parallel().filter(s -> s.getId().intValue() == id.intValue()).findAny();
 	}
 
+	// 修改
 	@Override
-	public int update(Course t) {
-		// TODO Auto-generated method stub
+	public int update(Course newCourse) {
+		// 驗證是否根據 id 值有此課程 ?
+		Optional<Course> optCourse = getById(newCourse.getId());
+		if (optCourse.isPresent()) {
+			// 注意：isPresent() 得到 true, 才可以使用 get() 得到 Course 物件
+			Course originalCourse = optCourse.get();
+			// 找到 originalCourse 在 courses 列表中的位置
+			int idx = IntStream.range(0, courses.size())
+					.filter(i -> courses.get(i).getId().intValue() == originalCourse.getId().intValue()).findFirst()
+					.getAsInt();
+			// 原位置idx的元素被新元素newCourse取代
+			courses.set(idx, newCourse); // 原位置idx的元素被新元素newCourse取代
+			return 1;
+		}
+		System.out.printf("CourseService.update() -> 無此代號: %d 的課程\n", newCourse.getId());
 		return 0;
-	}
-
-	@Override
-	public int deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isIdExists(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 	
+	// 刪除
+	@Override
+	public int deleteById(Integer id) {
+		// 驗證是否根據 id 值有此課程
+		Optional<Course> optCourse = getById(id);
+		if(optCourse.isPresent()) {
+			Course originalCourse = optCourse.get();
+			courses.remove(originalCourse); // 移除
+			return 1;
+		}
+		System.out.printf("CourseService.deleteById() -> 無此代號: %d 的課程\n", id);
+		return 0;
+	}
+	
+	// 判斷 id 是否存在 ?
+	@Override
+	public boolean isIdExists(Integer id) {
+		return courses.stream().parallel().filter(s -> s.getId() == id).findAny().isPresent();
+	}
+
 }
--- src/main/java/web/mvc/student/service/StudentService.java
+++ src/main/java/web/mvc/student/service/StudentService.java
@@ -19,14 +19,16 @@ public class StudentService implements IService<Student> {
 	}
 	
 	// 全部查詢
+	@Override
 	public List<Student> queryAll() {
 		return students;
 	}
 	
 	// 新增
+	@Override
 	public int add(Student student) {
 		if(isIdExists(student.getId())) {
-			System.out.printf("add() -> id: %d 已存在", student.getId());
+			System.out.printf("StudentService.add() -> id: %d 已存在", student.getId());
 			return 0; // 0:失敗
 		}
 		students.add(student);
@@ -34,12 +36,14 @@ public class StudentService implements IService<Student> {
 	}
 	
 	// 單筆查詢：根據 id 來取得 student 物件
+	@Override
 	public Optional<Student> getById(Integer id) {
 		// parallel 平行查詢/多核心同步運算
 		return students.stream().parallel().filter(s -> s.getId().intValue() == id.intValue()).findAny();
 	}
 	
 	// 修改 student
+	@Override
 	public int update(Student newStudent) {
 		// 驗證是否根據 id 值有此學生 ?
 		Optional<Student> optStudent = getById(newStudent.getId());
@@ -55,11 +59,12 @@ public class StudentService implements IService<Student> {
 			students.set(idx, newStudent); // 原位置idx的元素被新元素newStudent取代
 			return 1;
 		}
-		System.out.printf("update() -> 無此學號: %d 的學生\n", newStudent.getId());
+		System.out.printf("StudentService.update() -> 無此學號: %d 的學生\n", newStudent.getId());
 		return 0;
 	}
 	
 	// 刪除 student
+	@Override
 	public int deleteById(Integer id) {
 		// 驗證是否根據 id 值有此學生 ?
 		Optional<Student> optStudent = getById(id);
@@ -68,11 +73,12 @@ public class StudentService implements IService<Student> {
 			students.remove(originalStudent); // 移除
 			return 1;
 		}
-		System.out.printf("deleteById() -> 無此學號: %d 的學生\n", id);
+		System.out.printf("StudentService.deleteById() -> 無此學號: %d 的學生\n", id);
 		return 0;
 	}
 	
 	// 判斷 id 是否存在 ?
+	@Override
 	public boolean isIdExists(Integer id) {
 		return students.stream().parallel().filter(s -> s.getId() == id).findAny().isPresent();
 	}
