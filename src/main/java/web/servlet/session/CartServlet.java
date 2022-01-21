12030e2e68f258517260773ce497aed1f7af78ba
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-21 13:34:08 +0800

    Add files via upload

---
+++ src/main/java/web/servlet/session/CartServlet.java
@@ -0,0 +1,5 @@
+package web.servlet.session;
+
+public class CartServlet {
+
+}
---
+++ src/main/java/web/servlet/session/OrderServlet.java
@@ -0,0 +1,28 @@
+package web.servlet.session;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/session/order")
+public class OrderServlet extends HttpServlet {
+	
+	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+	
+}
