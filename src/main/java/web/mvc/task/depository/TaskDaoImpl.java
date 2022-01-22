68cf1ef36f740b64f4266fd9bd821fbb642da2c4
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-22 09:47:12 +0800

    Add files via upload

--- src/main/java/web/mvc/task/depository/TaskDaoImpl.java
+++ src/main/java/web/mvc/task/depository/TaskDaoImpl.java
@@ -1,11 +1,25 @@
 package web.mvc.task.depository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 import web.mvc.task.model.Task;
 
 public class TaskDaoImpl implements TaskDao {
-
+	private static final String DB_PATH = "/Users/vincenttuan/eclipse-workspace-javaexpert/web/src/main/java/web/mvc/task/depository";
+	private static final String DB_NAME = "/database.db";
+	private static Connection conn;
+	static {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			String fullPath = DB_PATH + DB_NAME;
+			conn = DriverManager.getConnection("jdbc:sqlite:" + fullPath);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
 	@Override
 	public Integer create(Task task) {
 		// TODO Auto-generated method stub
