625c59a05a9c7c8ad41bde5da1514d23cfb7635a
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-22 11:23:44 +0800

    Add files via upload

--- src/main/java/web/mvc/task/depository/TaskDaoImpl.java
+++ src/main/java/web/mvc/task/depository/TaskDaoImpl.java
@@ -2,6 +2,9 @@ package web.mvc.task.depository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import web.mvc.task.model.Task;
@@ -20,6 +23,12 @@ public class TaskDaoImpl implements TaskDao {
 		}
 	}
 	
+	/*
+	public static void main(String[] args) {
+		System.out.println("產生資料庫");
+	}
+	*/
+	
 	@Override
 	public Integer create(Task task) {
 		// TODO Auto-generated method stub
@@ -28,8 +37,29 @@ public class TaskDaoImpl implements TaskDao {
 
 	@Override
 	public List<Task> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Task> tasks = new ArrayList<>();
+		String sql = "select id, name, work, eat, commute, entertainment, sleep from Task";
+		try(Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);) {
+			
+			while (rs.next()) {
+				// 建立 task 物件並將資料表欄位資訊注入
+				Task task = new Task();
+				task.setId(rs.getInt("id"));
+				task.setName(rs.getString("name"));
+				task.setWork(rs.getInt("work"));
+				task.setEat(rs.getInt("eat"));
+				task.setCommute(rs.getInt("commute"));
+				task.setEntertainment(rs.getInt("entertainment"));
+				task.setSleep(rs.getInt("sleep"));
+				// 將 task 物件加入到 tasks 集合中
+				tasks.add(task);
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return tasks;
 	}
 
 	@Override
