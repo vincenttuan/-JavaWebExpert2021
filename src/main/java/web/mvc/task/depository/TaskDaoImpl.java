c83c7b084c05467dd89819e480ceb1c12c649b4d
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-22 09:36:41 +0800

    Add files via upload

---
+++ src/main/java/web/mvc/task/depository/TaskDaoImpl.java
@@ -0,0 +1,39 @@
+package web.mvc.task.depository;
+
+import java.util.List;
+
+import web.mvc.task.model.Task;
+
+public class TaskDaoImpl implements TaskDao {
+
+	@Override
+	public Integer create(Task task) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task readById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer update(Integer id, Task newTask) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer delete(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
